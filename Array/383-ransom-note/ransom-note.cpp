432f4229547e1029fc4d11014e1725fcf8c2f6ac
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2024-08-24 00:08:09 +0530

    Time: 23 ms (18.72%) | Memory: 11.7 MB (10.43%) - LeetSync

---
+++ Array/383-ransom-note/ransom-note.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int len1 = ransomNote.length();
+        int len2 = magazine.length();
+        map<char, int>mp;
+
+        if(len1>len2)
+        {
+            return false;
+        }
+
+        for(int i = 0; i<len2; i++)
+        {
+            mp[magazine[i]]++;
+        }
+
+        for(int i = 0; i<len1; i++)
+        {
+            mp[ransomNote[i]]--;
+        }
+        int count = 0;
+        for(int i = 0; i< len1; i++)
+        {
+            
+            if(mp[ransomNote[i]] >=0)
+            {
+                continue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
