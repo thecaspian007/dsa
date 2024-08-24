583bd8e5364288ec40e2b81a2fdd7d2e4ceef043
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2024-08-24 21:57:52 +0530

    Time: 350 ms (9.08%) | Memory: 134.7 MB (5.31%) - LeetSync

---
+++ Array/219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int len = nums.size();
+        map<int, int>mp;
+        map<int, vector<int>>np;
+
+        if(len == 0)
+        {
+            return false;
+        }
+
+        for(int i = 0; i<len; i++)
+        {
+            np[nums[i]].push_back(i);
+        }
+
+        for(auto a: np)
+        {
+             vector<int> val = a.second;
+             int p = 0;
+             if(val.size() >1)
+             {
+                for(int i = 1; i< val.size(); i++)
+                {
+                    if(abs(val[p] - val[i]) <= k )
+                    {
+                        return true;
+                    }
+                    else if(abs(val[p] - val[i]) > k )
+                    {
+                        p++;
+                    }
+                }
+             }
+        }
+        return false;
+
+    }
+};
