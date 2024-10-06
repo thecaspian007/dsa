b2902007ca727adcf9015f89c3318684298d3de3
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2024-10-06 18:47:06 +0530

    Time: 2 ms (41.14%) | Memory: 7.9 MB (37.52%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    
+    int trailingZeroes(int n) {
+       int result = 0;
+
+       for(int i = 5; n/i >0; i*=5)
+       {
+        result += n/i;
+       }
+       return result;
+    }
+};
