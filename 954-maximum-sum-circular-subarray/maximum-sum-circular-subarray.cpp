f61e13139a577903805e52939fd33629e1f355bb
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2024-10-10 21:52:45 +0530

    Time: 47 ms (44.55%) | Memory: 42.4 MB (92.87%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,54 +1,38 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int n=nums.size();
-        
-        int ans=INT_MIN;
-        
-        vector<int>l(n);
-        vector<int>r(n);
-        int s=nums[0];
-        l[0]=nums[0];
-        for(int i=1;i<n;i++)
+        int len = nums.size();
+        if(len == 0)
         {
-         s +=nums[i];
-         l[i]=max(s,l[i-1]);
+            return 0;
         }
 
-        //for right
-        s=nums[n-1];
-        r[n-1]=s;
-        for(int i=n-2;i>=0;i--)
-        {
-            s +=nums[i];
-            r[i]=max(s,r[i+1]);
-        }
+        int total = 0;
+        int temp_max = 0;
+        int temp_min = 0;
+        int min_sum = INT_MAX;
+        int max_sum = INT_MIN;
 
-        for(int i=0;i<n;i++)
+        for(int i = 0; i< len; i++)
         {
-            int curr;
-            curr =l[i];
-            if(i!=n-1)
+            total += nums[i];
+           temp_max += nums[i];
+            temp_min += nums[i];
+
+            if(temp_max < nums[i])
             {
-             curr +=r[i+1];
+                temp_max = nums[i];
             }
-            ans=max(ans,curr);
+
+            if(temp_min > nums[i])
+            {
+                temp_min = nums[i];
+            }
+            max_sum = max(max_sum, temp_max);
+            min_sum = min(min_sum, temp_min);
         }
 
-
-       int sum=0;
-        for(int i=0;i<n;i++)
-        {
-        sum +=nums[i];
-        ans=max(ans,sum);
-        if(sum<0)
-        {
-            sum=0;
-        }
-        }
-
-
-        return ans;
-
+        if(total == min_sum) return max_sum;
+        return max(max_sum, total-min_sum);
     }
 };
