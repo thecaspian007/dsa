379fe8c5bc1f9e69ddcd721d7627d94c632ba3ff
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2024-11-12 22:50:10 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (79.41%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool primeSubOperation(vector<int>& nums) {
+         int maxElement = *max_element(nums.begin(), nums.end());
+        
+        // Create Sieve of Eratosthenes array to identify prime numbers
+        vector<bool> sieve(maxElement + 1, true);
+        sieve[1] = false;
+        for (int i = 2; i <= sqrt(maxElement + 1); i++) {
+            if (sieve[i]) {
+                for (int j = i * i; j <= maxElement; j += i) {
+                    sieve[j] = false;
+                }
+            }
+        }
+        
+        // Check if array can be made strictly increasing by subtracting prime numbers
+        int currValue = 1;
+        int i = 0;
+        while (i < nums.size()) {
+            int difference = nums[i] - currValue;
+            
+            if (difference < 0) {
+                return false;
+            }
+            
+            if (sieve[difference] == true || difference == 0) {
+                i++;
+                currValue++;
+            } else {
+                currValue++;
+            }
+        }
+        return true;
+    }
+};
