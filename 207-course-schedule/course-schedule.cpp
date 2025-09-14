d8e90b00f4904b035c9b9a53943a1ca8a4b0b3b0
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2025-09-14 12:56:37 +0530

    Time: 3 ms (79.50%) | Memory: 19.2 MB (86.51%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -3,31 +3,23 @@ public:
     bool dfs(vector<vector<int>>& preList, int node, vector<int>& vis) {
         if (vis[node] == 1)
             return true;
-
         if (vis[node] == 2)
             return false;
-
         vis[node] = 1;
-
         for (int next : preList[node]) {
             if (dfs(preList, next, vis)) {
                 return true; 
             }
         }
-
         vis[node] = 2;
         return false;
     }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> preList(numCourses);
-
         for (const auto& prereq : prerequisites) {
             preList[prereq[0]].push_back(prereq[1]);
         }
-
         vector<int> vis(numCourses, 0);
-
         for (int i = 0; i < numCourses; i++) {
             if (vis[i] == 0) {
                 if (dfs(preList, i, vis)) {
@@ -35,7 +27,6 @@ public:
                 }
             }
         }
-
         return true;
     }
 };
