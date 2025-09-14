ac9012d64b32ddcf202dee6efc41bcae52e4850b
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2025-09-14 21:51:40 +0530

    Time: 3 ms (71.43%) | Memory: 15.7 MB (35.46%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void dfs(int openP, int closeP, string s, int n, vector<string>& res) {
+        if (openP == closeP && openP + closeP == n * 2) {
+            res.push_back(s);
+            return;
+        }
+        if (openP < n) {
+            dfs(openP + 1, closeP, s + "(", n, res);
+        }
+        if (closeP < openP) {
+            dfs(openP, closeP + 1, s + ")", n, res);
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        dfs(0, 0, "", n, res);
+        return res; 
+    }
+};
