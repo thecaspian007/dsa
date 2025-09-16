f82e86415a9c4babbccfa7c991d017dfaa74d6d3
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2025-09-16 23:41:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (80.62%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        unordered_map<int, int>mp;
-        int len = nums.size();
-
-        if(len == 0)
-        {
+        if(nums.size() == 0) {
             return 0;
         }
-
-        for(int i = 0; i<len; i++)
-        {
-            mp[nums[i]]++;
-        }
-
-        int k = 0;
-        int i = 0;
-        for(auto it: mp)
-        {
-            if(it.first == val)
-            {
-                k = it.second;
+        cout<<"nums.size() = "<< nums.size()<< endl;
+        int index = 0, count=0;
+        for(int i = 0; i<nums.size(); i++) {
+            if(nums[i] == val) {
+                count++;
+                continue;
             }
-            else
-            while(it.second--)
-            {
-                nums[i] = it.first;
-                i++;
-            }
+            nums[index] = nums[i];
+            index++;
+           
         }
-        return len-k;
+        return index;
     }
 };
