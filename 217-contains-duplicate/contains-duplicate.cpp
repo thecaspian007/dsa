615a58ace2249227a463d6f645b6b8f2c395156a
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2025-09-25 00:24:27 +0530

    Time: 56 ms (32.86%) | Memory: 93 MB (15.74%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int len = nums.size();
+        if(len == 0){
+            return false;
+        }
+        unordered_map<int, int>mp;
+        for(int i = 0; i< len; i++){
+            mp[nums[i]]++;
+        }
+        for(auto it: mp){
+            if(it.second>1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
