bef0ff260d0d09c71c9342b9cad0798490ce70bd
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2025-09-26 00:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (43.94%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inOrder(TreeNode* root, int &count){
+        if(root == nullptr){
+            return;
+        }
+        inOrder(root->left, count);
+        count++;
+        inOrder(root->right, count);
+    }
+    int countNodes(TreeNode* root) {
+        if(root == nullptr){
+            return 0;
+        }
+        int count = 0;
+        inOrder(root, count);
+        return count;
+    }
+};
