45a7adef54bcdcac28434fa8d467eddf5169ed46
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2025-10-06 22:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (17.92%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -4,9 +4,8 @@ public:
         if(n == 0) {
             return false;
         }
-        while(n%2==0){
-            n/=2;
-        }
-        return n==1;
+        if (n == 1) return true;
+        if (n % 2 != 0) return false; 
+        return isPowerOfTwo(n/2);
     }
 };
