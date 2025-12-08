428447431d9a4501a0f387d170115a27de8d426a
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2025-12-09 00:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (18.58%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -6,10 +6,11 @@ public:
         }
         int index = 1;
         for(int i = 1; i<nums.size(); i++) {
-            if(nums[i] != nums[i-1]) {
-                nums[index] = nums[i];
-                index++;
+            if(nums[i] == nums[i-1]) {
+               continue;
             }
+            nums[index] = nums[i];
+            index++;
         }
         return index;
     }
