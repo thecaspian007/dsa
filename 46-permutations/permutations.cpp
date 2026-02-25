2fc5be3743928f0f0d9624d24ee1dd0f67afd672
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-02-25 10:00:04 +0530

    Time: 3 ms (27.58%) | Memory: 10.7 MB (41.30%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void permuteRecursive(vector<int> &num, int begin, vector<vector<int>> &result)	{
+		if (begin >= num.size()) {
+		    result.push_back(num);
+		    return;
+		}
+		for (int i = begin; i < num.size(); i++) {
+		    swap(num[begin], num[i]);
+		    permuteRecursive(num, begin + 1, result);
+		    swap(num[begin], num[i]);
+		}
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+	    permuteRecursive(nums, 0, result);
+	    return result;
+    }
+};
