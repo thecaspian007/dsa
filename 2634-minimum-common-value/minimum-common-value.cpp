6b919c97c30ec41e0434b2bc6cff5d4db2f2c7bf
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-05-20 01:07:59 +0530

    Time: 53 ms (9.91%) | Memory: 69.7 MB (14.62%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() == 0 || nums2.size() == 0){
+            return -1;
+        }
+        unordered_map<int, int>mp;
+        for(int i = 0; i< nums1.size(); i++){
+            mp[nums1[i]]++;
+        }
+
+        for(int i = 0; i< nums2.size(); i++){
+            if(mp.find(nums2[i]) != mp.end()){
+                return nums2[i];
+            }
+        } 
+        return -1;
+    }
+};
