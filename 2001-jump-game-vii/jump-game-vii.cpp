028853268b29f19051338d16776ab438a6184cbb
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-05-25 09:52:41 +0530

    Time: 4 ms (91.39%) | Memory: 20 MB (88.38%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+
+        vector<bool> dp(n, false);
+        dp[0] = true;
+
+        int reachable = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            // Add new index entering window
+            if (i - minJump >= 0 && dp[i - minJump]) {
+                reachable++;
+            }
+
+            // Remove old index leaving window
+            if (i - maxJump - 1 >= 0 && dp[i - maxJump - 1]) {
+                reachable--;
+            }
+
+            // Current index reachable
+            if (s[i] == '0' && reachable > 0) {
+                dp[i] = true;
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
