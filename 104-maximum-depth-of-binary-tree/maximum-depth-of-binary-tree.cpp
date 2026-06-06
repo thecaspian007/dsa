fd2a130ceb84bdd7fa34ec3cdbe13344fc1b723a
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-06-07 01:00:42 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (77.02%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -15,7 +15,6 @@ public:
         if(root == nullptr){
             return 0;
         }
-
         int lh = maxDepth(root->left);
         int rh = maxDepth(root->right);
         return 1 + max(lh, rh);
