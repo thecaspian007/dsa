f1f32941ae04cb17a000b7cd1063b4dcefbcf34c
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-06-09 00:57:08 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (56.14%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,19 +11,24 @@
  */
 class Solution {
 public:
-    TreeNode* helper(vector<int>& post, unordered_map<int,int>& inMap, int& idx, int start, int end) {
-        if (start > end) return nullptr;
-        int val = post[idx--];
-        TreeNode* root = new TreeNode(val);
-        int pos = inMap[val];
-        root->right = helper(post, inMap, idx, pos + 1, end);
-        root->left = helper(post, inMap, idx, start, pos - 1);
+    unordered_map<int, int>mp;
+    int postIndex;
+    TreeNode* solve(vector<int>& inorder, vector<int>& postorder, int start, int end){
+        if(start>end){
+            return nullptr;
+        }
+        int rootVal = postorder[postIndex--];
+        TreeNode* root = new TreeNode(rootVal);
+        int pos = mp[rootVal];
+        root->right = solve(inorder, postorder, pos+1, end);
+        root->left = solve(inorder, postorder, start, pos-1);
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        unordered_map<int,int> inMap;
-        for (int i = 0; i < inorder.size(); i++) inMap[inorder[i]] = i;
-        int idx = postorder.size() - 1;
-        return helper(postorder, inMap, idx, 0, inorder.size() - 1);
+        for(int i = 0; i<inorder.size(); i++){
+            mp[inorder[i]] = i;
+        }
+        postIndex = postorder.size()-1;
+        return solve(inorder, postorder, 0, inorder.size()-1);
     }
 };
