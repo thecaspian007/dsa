a12a4de1304af4419c66a557f0e620409ef0ff66
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-06-23 01:45:47 +0530

    Time: 8 ms (23.00%) | Memory: 22 MB (79.37%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+     bool isPalindrome(string &s, int l, int r)
+    {
+        while(l < r)
+        {
+            if(s[l] != s[r])
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+        int l = 0;
+        int r = s.size() - 1;
+        while(l < r)
+        {
+            if(s[l] == s[r])
+            {
+                l++;
+                r--;
+            }
+            else
+            {
+                return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+            }
+        }
+        return true;
+    }
+};
