31763cca790c438f455d409bd684a6c8330b44e1
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-06-30 00:47:03 +0530

    Time: 7 ms (51.54%) | Memory: 15.1 MB (70.60%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -19,26 +19,18 @@ public:
     Node* copyRandomList(Node* head) {
         if (!head)
             return nullptr;
-
         unordered_map<Node*, Node*> ans;
-
         Node* temp = head;
-
         while (temp) {
             ans[temp] = new Node(temp->val);
-
             temp = temp->next;
         }
-
         temp = head;
-
         while (temp) {
             ans[temp]->next = ans[temp->next];
             ans[temp]->random = ans[temp->random];
-
             temp = temp->next;
         }
-
         return ans[head];
     }
 };
