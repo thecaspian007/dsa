692bfa28efa08945438d39217f2962fbcc0894e2
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-06-30 09:31:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (16.37%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    int fib(int n) {
-       if(n== 0){
-        return 0;
-       }
-       if(n== 1){
-        return 1;
-       }
+    int solve(int n, vector<int> &dp){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        return dp[n];
+    }
 
-       return fib(n-1) + fib(n-2);
+    int fib(int n) {
+       vector<int> dp(n+1, -1);
+       return solve(n, dp);
     }
 };
