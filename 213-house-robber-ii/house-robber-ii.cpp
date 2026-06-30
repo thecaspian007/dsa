635c5f6363230d3a265030085a1d6060e9415fca
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-06-30 22:18:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (18.54%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int solve(vector<int> &nums){
-        int n = nums.size();
-        if(n == 1){
-            return nums[0];
-        }
-        vector<int>dp(n+1);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for(int i = 2; i<n; i++){
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
-        }
-        return dp[n-1];
+    int solve(int i, int end, vector<int>& nums, vector<int>& dp)
+    {
+        if (i > end)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        int rob = nums[i] + solve(i + 2, end, nums, dp);
+        int skip = solve(i + 1, end, nums, dp);
+        return dp[i] = max(rob, skip);
     }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n==1){
+        if (n == 1)
             return nums[0];
-        }
-        vector<int>first(nums.begin(), nums.end()-1);
-        vector<int>second(nums.begin()+1, nums.end());
-        return max(solve(first), solve(second));
+        vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
+        int case1 = solve(0, n - 2, nums, dp1); 
+        int case2 = solve(1, n - 1, nums, dp2);
+        return max(case1, case2);
     }
 };
