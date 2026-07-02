eb06e073de473d42e9297a726be2071f6e2cfdb7
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-07-02 10:18:08 +0530

    Time: 67 ms (64.42%) | Memory: 11.9 MB (53.77%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int minCut(string s) {
-        int n = s.length();
+        int n = s.size();
         vector<vector<bool>> isPal(n, vector<bool>(n, false));
-        for(int i = n - 1; i >= 0; i--) {
-            for(int j = i; j < n; j++) {
-                if(s[i] == s[j] &&
-                   (j - i <= 2 || isPal[i+1][j-1])) {
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (len == 1)
                     isPal[i][j] = true;
+                else if (len == 2)
+                    isPal[i][j] = (s[i] == s[j]);
+                else
+                    isPal[i][j] = (s[i] == s[j]) && isPal[i + 1][j - 1];
+            }
+        }
+        vector<int> dp(n + 1, 0);
+        dp[n] = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = INT_MAX;
+            for (int j = i; j < n; j++) {
+                if (isPal[i][j]) {
+                    dp[i] = min(dp[i], 1 + dp[j + 1]);
                 }
             }
         }
-        vector<int> dp(n);
-        for(int i = 0; i < n; i++) {
-            if(isPal[0][i]) {
-                dp[i] = 0;
-            } else {
-                dp[i] = i; 
-                for(int j = 1; j <= i; j++) {
-                    if(isPal[j][i]) {
-                        dp[i] = min(dp[i], dp[j-1] + 1);
-                    }
-                }
-            }
-        }
-        return dp[n-1];
+        return dp[0];
     }
 };
