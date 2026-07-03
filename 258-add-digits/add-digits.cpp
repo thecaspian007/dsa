33e19a059fcbe0bae7da18fa6a189e187e1ad2e6
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-07-04 00:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (80.07%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num == 0){
+            return 0;
+        }
+        if(num/10 == 0){
+            return num;
+        }
+
+        int sum  = 0;
+        while(num>0){
+            sum += num%10;
+            num = num/10;
+        }
+        return addDigits(sum);
+    }
+};
