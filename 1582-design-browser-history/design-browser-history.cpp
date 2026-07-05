36636e2300bb35c23a0e6f5cd09f0cebebb68df5
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-07-06 00:12:42 +0530

    Time: 83 ms (12.78%) | Memory: 94.3 MB (15.09%) - LeetSync

---
+++ 1582-design-browser-history/design-browser-history.cpp
@@ -0,0 +1,45 @@
+class BrowserHistory {
+public:
+    stack<string> backStack;
+    stack<string> forwardStack;
+    string current;
+
+    BrowserHistory(string homepage) {
+        current = homepage;
+    }
+    
+    void visit(string url) {
+        backStack.push(current);
+        current = url;
+        while (!forwardStack.empty())
+            forwardStack.pop();
+    }
+    
+    string back(int steps) {
+       while (steps > 0 && !backStack.empty()) {
+            forwardStack.push(current);
+            current = backStack.top();
+            backStack.pop();
+            steps--;
+        }
+        return current; 
+    }
+    
+    string forward(int steps) {
+        while (steps > 0 && !forwardStack.empty()) {
+            backStack.push(current);
+            current = forwardStack.top();
+            forwardStack.pop();
+            steps--;
+        }
+        return current;
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
