f074da6238054ad4791434d8c18e25269044dccf
Sumeet Singh <133153246+thecaspian007@users.noreply.github.com>
2026-07-05 16:11:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (30.75%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        for(int i = 0; i < n; i++)
+            dp[i][i] = nums[i];
+        for(int len = 2; len <= n; len++)
+        {
+            for(int l = 0; l + len - 1 < n; l++)
+            {
+                int r = l + len - 1;
+                dp[l][r] = max(nums[l] - dp[l + 1][r],nums[r] - dp[l][r - 1]);
+            }
+        }
+        return dp[0][n - 1] >= 0;
+    }
+};
